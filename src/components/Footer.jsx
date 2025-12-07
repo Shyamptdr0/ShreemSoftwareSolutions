328cce9f5b34e86951767e066d54e72eea2de8b9
Shyam Patidar <anu.shyam.patidar@gmail.com>
2025-12-07 20:31:40 +0530

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React from "react";
+
+export default function FooterPage() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Footer Page</h1>
+    </div>
+  );
+}
